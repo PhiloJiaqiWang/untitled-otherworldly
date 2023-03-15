bb0a2615d87eaee0403911e50a82139e2d58a65b
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2023-03-15 20:41:03 +0000

    player setup

--- game.js
+++ game.js
@@ -1,15 +1,24 @@
-let tileMap = new Tileset("./assets/tilemap_packed.png", 16, 12, 11, 10)
+let tileMap = new Tileset("./assets/tilemap_packed.png", 16, 12, 11, 20)
+
+let playerTileset = new Tileset("./assets/little_boy.png", 16, 3, 4, 20)
+
+let player = new Entity(playerTileset, 50)
 
 function preload() {
   tileMap.preload()
+  player.preload()
 }
 
 function setup() {
   createCanvas(500, 500)
   noSmooth()
+  player.addAnimation('walkLeft', [9,10,11,10], 10)
+  player.setCurrentAnimation('walkLeft')
+  frameRate(60)
 }
 
 function draw() {
+  background(0)
   tileMap.drawMap([
     [0,1,0,0,1,0],
     [0,2,1,0,1,0],
@@ -19,4 +28,6 @@ function draw() {
     [0,1,0,1,1,0],
     [0,2,1,0,0,0]
   ])
+  player.setPosition({x: 30, y: 30})
+  player.draw()
 }
